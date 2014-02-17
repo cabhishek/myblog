6a5996a8ae7ff3df2bc5718594bbcc9d907d3175
Abhishek Kapatkar <cabhishek@gmail.com>
2014-02-17 16:36:29 -0500

    format

--- admin.py
+++ admin.py
@@ -6,22 +6,20 @@ import pageshandler
 
 post_deploy.run_deploy_task()
 
-
 application = webapp.WSGIApplication([
-  ('/admin/', handlers.AdminHandler),
-  ('/admin/newpost', handlers.PostHandler),
-  ('/admin/post/(\d+)', handlers.PostHandler),
-  ('/admin/regenerate', handlers.RegenerateHandler),
-  ('/admin/flushmemcache', handlers.FlushMemCacheHandler),
-  ('/admin/pagepost/(\w+)', pageshandler.PageHandler),
-  ('/admin/post/delete/(\d+)', handlers.DeleteHandler)
+      ('/admin/', handlers.AdminHandler),
+      ('/admin/newpost', handlers.PostHandler),
+      ('/admin/post/(\d+)', handlers.PostHandler),
+      ('/admin/regenerate', handlers.RegenerateHandler),
+      ('/admin/flushmemcache', handlers.FlushMemCacheHandler),
+      ('/admin/pagepost/(\w+)', pageshandler.PageHandler),
+      ('/admin/post/delete/(\d+)', handlers.DeleteHandler)
 ])
 
 
 def main():
-  run_wsgi_app(application)
+    run_wsgi_app(application)
 
 
 if __name__ == '__main__':
-  main()
-
+    main()
